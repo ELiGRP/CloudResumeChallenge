3720811abd77fd8d1e1d2d2816fcb2dea4a9bc2c
Elijah <elijahdouglas1997@gmail.com>
2022-08-16 09:33:52 -0400

    cy test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,13 @@ describe('My First Test', () => {
   it('Visits the Kitchen Sink', () => {
     cy.visit('https://elidouglas.com/')
 
-    cy.get('Visitors')
+    cy.get('#count')
+      .invoke('text')
+      .then(Number)
+      .then((n) => {
+        cy.reload()
+        // check the incremented value
+        cy.contains('#count', String(n + 1))
   })
+})
 })
