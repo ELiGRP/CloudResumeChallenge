8e888c0ba6d2d63db67f51a702fe79905bac409c
Elijah <elijahdouglas1997@gmail.com>
2022-08-22 07:26:19 -0400

    const test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,22 @@
 describe('My First Test', () => {
   it('Visits the my website', () => {
     cy.visit('https://elidouglas.com/')
+
     cy.wait(500)
-    cy.get('#count')
-    .then(($count) => {
-      var num1 = parseFloat($count.text())
+
+    cy.get('#count').then(($count) => {
+      const num1 = parseFloat($count.text())
       console.log(num1)
+      cy.reload()
+      cy.wait(500)
+
+      cy.get('#count').then(($count) => {
+        
+        const num2 = parseFloat($count.text())
+        console.log(num2)
+
+        expect(num2).to.eq(num1 + 1)
+      })
     })
   })
 })
